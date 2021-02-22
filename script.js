a2697d74a71a59f89d69920cfa970b572e45099e
Lucas Gelinas <lucas.gelinas@ocsbstudent.ca>
2021-02-22 15:28:30 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// Connecting button to the function
+document.getElementById('button').addEventListener('click', check)
+let age = 0
+let day = 0
+
+function check () {
+  // Taking user input to display appropriate message
+  age = document.getElementById('userage').value
+  age = parseInt(age)
+  day = document.getElementById('userday').value
+  day = parseInt(day)
+
+  if ((day == 'Tuesday' || day == 'Thursday') || (age > 12 && age < 21) {
+    document.getElementById('response').innerHTML = 'You are eligble for a discount today!'
+  } else {
+    document.getElementById('response').innerHTML = 'You will need to pay full price today.'
+  }
+}
