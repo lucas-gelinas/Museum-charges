4ae00a23f53a6e75073b606c709c4d4d28cfd2cf
Lucas Gelinas <lucas.gelinas@ocsbstudent.ca>
2021-02-22 16:05:30 +0000

    Fixed final mishap

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ function check () {
   age = document.getElementById('userage').value
   age = parseInt(age)
   day = document.getElementById('userday').value
-  day = parseInt(day)
 
   if ((day === 'Tuesday' || day === 'Thursday') || (age > 12 && age < 21)) {
     document.getElementById('response').innerHTML = 'You are eligble for a discount today!'
